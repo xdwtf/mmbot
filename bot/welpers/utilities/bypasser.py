3bc052540ada293e81537b6fbecad5e5f85d56a4
Lylia <71930916+dishapatel010@users.noreply.github.com>
2022-03-15 12:43:25 +0530

    update bypasser.py

--- bot/welpers/utilities/bypasser.py
+++ bot/welpers/utilities/bypasser.py
@@ -222,21 +222,16 @@ def remove_dup(lst):
     set1 = set()
     final_list = []
     for i in lst:
-        set1.add(i)
-    for i in set1:    
-        final_list.append(i)
-        if final_list:
-            try:
-                if len(final_list) > 4096:
-                    file_write = open(f'cyberdrop.txt', 'a+')
-                    file_write.write(f"{final_list}")
-                    file_write.close()
-                    message = await update.reply_document(
-                         f"cyberdrop.txt",
-                         disable_notification=True,
-                         quote=True
-                    )
-                    os.remove(f"cyberdrop.txt")
-                else:
-                     message = await update.reply_text(
-                        text=final_list, disable_web_page_preview=True, quote=True)
+      set1.add(i)
+    for i in set1:
+      final_list.append(i)
+      if final_list:
+        try:
+          if len(final_list) > 4096:
+            file_write = open(f'cyberdrop.txt', 'a+')
+            file_write.write(f"{final_list}")
+            file_write.close()
+            message = await update.reply_document(f"cyberdrop.txt", disable_notification=True, quote=True)
+            os.remove(f"cyberdrop.txt")
+            else:
+              message = await update.reply_text(text=final_list, disable_web_page_preview=True, quote=True)
